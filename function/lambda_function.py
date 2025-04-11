646d368b7d62ad5e1231946302172ee661d2e972
Nadir Metrouh <mnadir20@gmail.com>
2025-04-11 18:32:19 +0200

    Update lambda_function.py

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -8,16 +8,12 @@ def lambda_handler(event, context):
     Returns:
         string: greeting response
     """
-    print('Starting functions\n-----------------------------------------------'
+    print('Starting functions\n---------------------------------------------'
 
     if event["input"] == "Hello":
-
-        return "World"
-
-    if event["input"] == "Hi":
         
-        return "Hi There"
-
+        return "World"
+        
     else:
-
-        raise
+        
+        raise ValueError("Invalid input: Only 'Hello' is supported.")
